836bcfe40d5b74a3edc210af6f1b8d61e835c65c
tomasdargis <167984812+tomasdargis@users.noreply.github.com>
2024-08-06 18:00:38 +0300

    Hero style

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,23 @@
+import './Hero.css';
 import logo from '/vite.svg'
 
 
-const imgStyle={height: '15rem'};
-
+const imgStyle={
+  height: '24rem',
+  width:"100%",
+};
+const title='Vite Next Generation Fronted Tooling'
 export function Hero(){
     return(
-        <section>
-      <div>
-        <h1>Vite Next Generation Fronted Tooling</h1>
-        <p>Get ready for a development environment that can finally catch up with you.</p>
-        <div>
-          <a href="#">Get Started</a>
-          <a href="#">Why Vite</a>
-          <a href="#">View on GitHub</a>
-          <a href="#"> ⚡️ ViteConf 24!</a>
+        <section className='hero'>
+      <div className='textContent'>
+        <h1 className='title'>{title}</h1>
+        <p className='description'>Get ready for a development environment that can finally catch up with you.</p>
+        <div className='btnList'>
+          <a className='btn' href="#">Get Started</a>
+          <a className='btn' href="#">Why Vite</a>
+          <a className='btn' href="#">View on GitHub</a>
+          <a className='btn' href="#"> ⚡️ ViteConf 24!</a>
         </div>
       </div>
       <img src={logo}style={imgStyle} alt="Vite Logo" />
